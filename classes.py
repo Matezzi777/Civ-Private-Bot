b547c7b3ccae40bef2f9aaaa5b27faece41fff63
Matezzi777 <matezzi777@gmail.com>
2024-02-26 06:54:49 +0100

    SuccessEmbed() and ErrorEmbed() added + CivPrivateBotEmbed() renamed to BotEmbed()

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 #================================================== BOT =====================================================
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="$", intents=discord.Intents.all(), description="Civ Private Bot v2.0.0")
+        super().__init__(command_prefix="$", intents=discord.Intents.all(), description="Civ Private Bot v2.0")
 
     async def setup_hook(self):
         await self.tree.sync()
@@ -15,8 +15,34 @@ class Bot(commands.Bot):
         print(f"{self.user.id} successfully logged in as {self.user.name}.\nRock n'Roll !")
 
 #================================================= EMBEDS ===================================================
-class CivPrivateBotEmbed(discord.Embed):
+class BotEmbed(discord.Embed):
     def __init__(self, *, colour=discord.Colour.purple(), color=discord.Colour.purple(), title=None, type='rich', url=None, description=None, timestamp=None) -> None:
+        super().__init__(
+            colour=colour,
+            color=color,
+            title=title,
+            type=type,
+            url=url,
+            description=description,
+            timestamp=timestamp
+            )
+        self.set_footer(text="Civ Private Bot 2.0")
+
+class SuccessEmbed(discord.Embed):
+    def __init__(self, *, colour=discord.Colour.green(), color=discord.Colour.green(), title="SUCCESS", type='rich', url=None, description=None, timestamp=None) -> None:
+        super().__init__(
+            colour=colour,
+            color=color,
+            title=title,
+            type=type,
+            url=url,
+            description=description,
+            timestamp=timestamp
+            )
+        self.set_footer(text="Civ Private Bot 2.0")
+
+class ErrorEmbed(discord.Embed):
+    def __init__(self, *, colour=discord.Colour.red(), color=discord.Colour.red(), title="ERROR", type='rich', url=None, description=None, timestamp=None) -> None:
         super().__init__(
             colour=colour,
             color=color,
