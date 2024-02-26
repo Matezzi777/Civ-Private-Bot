7e58e4da8d14ea6087b42e0074cd81715ebc15cd
Matezzi777 <matezzi777@gmail.com>
2024-02-26 07:01:13 +0100

    clear() updated, setup_leaderboard changed and renamed to update_leaderboard() + New Embed supported

--- CivPrivateBot.py
+++ CivPrivateBot.py
@@ -7,7 +7,7 @@ from birthdays import *
 from draft import *
 from mapvote import *
 from ranked import *
-from classes import Bot, CivPrivateBotEmbed
+from classes import Bot, BotEmbed, SuccessEmbed, ErrorEmbed
 from tokens import TOKEN
 
 #Définition du bot
@@ -62,10 +62,10 @@ class ReportButton(discord.ui.Button):
         if (self.count >= needed_confirm):
             self.label="Report validated"
             self.disabled=True
-            await valid_report(self.liste_players)
-            embed=CivPrivateBotEmbed(colour=discord.Colour.green(), title="Result confirmed", description="Result confirmed, result stored in the database and player's stats updated.")
+            embed=SuccessEmbed(description="Result confirmed, result stored in the database and player's stats updated.")
             await interaction.response.edit_message(view=self.view)
             await interaction.followup.send(embed=embed)
+            await valid_report(bot, self.liste_players)
         else:
             self.label=f"{needed_confirm-self.count} more ✅ needed"
             await interaction.response.edit_message(view=self.view)
@@ -74,28 +74,33 @@ class ReportButton(discord.ui.Button):
 #$ping
 @bot.command()
 async def ping(ctx : commands.Context) -> None:
-    embed = discord.Embed(colour=discord.Colour.green(), title="PONG")
+    embed = SuccessEmbed(title="PONG")
+    embed.remove_footer()
     await ctx.send(embed=embed)
 #$hello
 @bot.command()
 async def hello(ctx : commands.Context) -> None:
     server = ctx.guild
-    embed = CivPrivateBotEmbed(title="Hello *Civ enjoyer !*", description=f"Let me introduce myself...\nMy name is **Civ Private Bot** (but you can call me CPB), I try my best to add some useful tools on the **{server.name}**, like $mapvote, $draft.\n\nWe actualy are working on an elo ranking system 😉\n\nPlease message <@866997795993944084> if you have any suggestions or feedback !")
+    embed = BotEmbed(title="Hello *Civ enjoyer !*", description=f"Let me introduce myself...\nMy name is **Civ Private Bot** (but you can call me CPB), I try my best to add some useful tools on the **{server.name}**, like $mapvote, $draft.\n\nWe actualy are working on an elo ranking system 😉\n\nPlease message <@866997795993944084> if you have any suggestions or feedback !")
     await ctx.send(embed=embed)
 #$serverinfo
 @bot.command()
 async def serverinfo(ctx : commands.Context) -> None:
     server = ctx.guild
-    embed = CivPrivateBotEmbed(title=f"{server.name}", description=f"Are you tired about leavers in Civ 6 Multiplayer ?\nAre you tired about the 3k gametime player bullying you ?\n\nWelcome to the {server.name} !\n\nNo worries, you are at the good place. This discord exists to gather a community of nice peoples who want to play civ, learn civ and discuss about civ !\n\nIf you are new here, don't worry, I'm sure you'll find what you are looking for.")
+    embed = BotEmbed(title=f"{server.name}", description=f"Are you tired about leavers in Civ 6 Multiplayer ?\nAre you tired about the 3k gametime player bullying you ?\n\nWelcome to the {server.name} !\n\nNo worries, you are at the good place. This discord exists to gather a community of nice peoples who want to play civ, learn civ and discuss about civ !\n\nIf you are new here, don't worry, I'm sure you'll find what you are looking for.")
     embed.add_field(name="Some stats about us :", value =f"Members : **{server.member_count}**\nText Channels : {len(server.text_channels)}\nVoice Channels : {len(server.voice_channels)}", inline=False)
     embed.add_field(name="", value=f"Created by <@866997795993944084> and <@828975075951902733> the {server.created_at.date()}", inline=False)
     await ctx.send(embed=embed)
 #$clear X
 @bot.command()
-async def clear(ctx : commands.Context, nombre : int) -> None:
-    async for message in ctx.channel.history(limit=nombre+1):
-        print(message)
+async def clear(ctx : commands.Context, n : int) -> None:
+    i : int = 0
+    async for message in ctx.channel.history(limit=n+1):
+        if (i != 0):
+            print(f"Message deleted from #{ctx.channel.name} ({i}/{n})")
         await message.delete()
+        i = i + 1
+    print(f"#{ctx.channel.name} cleaned.")
 #$datenow
 @bot.command()
 async def datenow(ctx : commands.Context) -> None:
@@ -134,36 +139,36 @@ async def set_birthday(ctx : commands.Context, date : str) -> None:
     user = ctx.message.author
     date_parsed = parse_date(date)
     if (date_parsed == "00/00"):
-        embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description="Wrong date format.\nUse the following format : DD/MM.")
+        embed= ErrorEmbed(description="Wrong date format.\nUse the following format : DD/MM.")
         return await ctx.send(embed=embed, delete_after=7)
     if (check_birthday(user)):
         actual_birthday = get_birthday(user)
         if (actual_birthday == date_parsed):
-            embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description=f"Your birthday is already stored in the database at **{actual_birthday}** (DD/MM).")
-            return await ctx.send(embed=embed, delete_after=7)
+            embed= ErrorEmbed(description=f"Your birthday is already stored in the database at **{actual_birthday}** (DD/MM).")
+            return await ctx.send(embed=embed)
         else:
             async def button_yes_callback(interaction : discord.Interaction):
                 change_birthday(user, date)
-                embed= discord.Embed(colour=discord.Colour.green(), title="Success", description=f"Date modified.")
+                embed= SuccessEmbed(description=f"Date modified.")
                 return await interaction.response.send_message(embed=embed)
             async def button_no_callback(interaction : discord.Interaction):
-                embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description=f"Date not modified.")
+                embed= ErrorEmbed(title="PROCESS CANCELED", description=f"Date not modified.")
                 return await interaction.response.send_message(embed=embed)
             button_yes.callback = button_yes_callback
             button_no.callback = button_no_callback
-            embed= CivPrivateBotEmbed(title="Modify birthday ?", description=f"Your birthday is already stored in the database at **{actual_birthday}** (DD/MM).\n\nDo you want to change it for *{date_parsed}* ?")
+            embed= BotEmbed(title="Modify birthday ?", description=f"Your birthday is already stored in the database at **{actual_birthday}** (DD/MM).\n\nDo you want to change it for *{date_parsed}* ?")
             return await ctx.send(embed=embed, view=yesno, delete_after=7)
     else:
         async def button_yes_callback(interaction : discord.Interaction):
             add_birthday(user, date)
-            embed= discord.Embed(colour=discord.Colour.green(), title="Success", description="Birthday added.")
+            embed= SuccessEmbed(description="Birthday added.")
             return await interaction.response.send_message(embed=embed)
         async def button_no_callback(interaction : discord.Interaction):
-            embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description="Birthday not added.")
+            embed= ErrorEmbed(title="PROCESS CANCELED", description="Birthday not added.")
             return await interaction.response.send_message(embed=embed)
         button_yes.callback = button_yes_callback
         button_no.callback = button_no_callback
-        embed=CivPrivateBotEmbed(title="Add birthday ?", description=f"You birthday is not on the database.\n\nDo you want to set it to *{date_parsed}* ?")
+        embed=BotEmbed(title="Add birthday ?", description=f"You birthday is not on the database.\n\nDo you want to set it to *{date_parsed}* ?")
         await ctx.send(embed=embed, view=yesno, delete_after=7)
 #$rem_birthday
 @bot.command()
@@ -175,17 +180,17 @@ async def rm_birthday(ctx : commands.Context) -> None:
     if (check_birthday(user)):
         async def button_yes_callback(interaction : discord.Interaction):
             rem_birthday(user)
-            embed= discord.Embed(colour=discord.Colour.green(), title="Success", description=f"Birthday deleted from database.")
+            embed= SuccessEmbed(description=f"Birthday deleted from database.")
             return await interaction.response.send_message(embed=embed)
         async def button_no_callback(interaction : discord.Interaction):
-            embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description=f"Birthday still stored in the database.")
+            embed= ErrorEmbed(title="PROCESS CANCELED", description=f"Birthday still stored in the database.")
             return await interaction.response.send_message(embed=embed)
         button_yes.callback = button_yes_callback
         button_no.callback = button_no_callback
-        embed= CivPrivateBotEmbed(title="Remove birthday ?", description="Do you really want to remove your birthday from the database ?")
+        embed= BotEmbed(title="Remove birthday ?", description="Do you really want to remove your birthday from the database ?")
         return await ctx.send(embed=embed, view=yesno, delete_after=10)
     else:
-        embed= discord.Embed(colour=discord.Colour.red(), title="Process aborted", description="No birthday found in the database for you.")
+        embed= ErrorEmbed(description="No birthday found in the database for you.")
         return await ctx.send(embed=embed)
 #$birthdays
 @bot.command()
@@ -197,9 +202,9 @@ async def birthdays(ctx : commands.Context) -> None:
 @bot.command()
 async def report(ctx : commands.Context, *args : discord.User) -> None:
     if (len(args) < 2):
-        embed=CivPrivateBotEmbed(colour=discord.Colour.red(), title="Process aborted", description="Not enough players in the game reported (minimum 2)")
+        embed=ErrorEmbed(description="Not enough players in the game reported (minimum 2)")
         return await ctx.send(embed=embed)
-    embed=CivPrivateBotEmbed(title="Verify the Result", description="Please make sure the result reported is correct before verifying it.\nThe report needs to be confirmed by 3 different players (2 for a 1v1).")
+    embed=BotEmbed(title="Verify the Result", description="Please make sure the result reported is correct before verifying it.\nThe report needs to be confirmed by 3 different players (2 for a 1v1).")
     i : int = 0
     message = ""
     while (i < len(args)):
@@ -213,14 +218,14 @@ async def report(ctx : commands.Context, *args : discord.User) -> None:
 async def stats(ctx : commands.Context) -> None:
     user : discord.User = ctx.message.author
     if (not is_player_in_database(user)):
-        embed=CivPrivateBotEmbed(colour=discord.Colour.red(), title="User not found", description=f"<@{user.id}> not found in the database.\nPlease report a game with this player to create an entry.")
+        embed=ErrorEmbed(description=f"{user.mention} not found in the database.\nPlay a game and report the result to create an entry.")
         return await ctx.send(embed=embed)
     elo : int = get_elo(user)
     top1 : int = get_top1(user)
     wins : int = get_wins(user)
     lost : int = get_lost(user)
     date : str = get_date(user)
-    embed=CivPrivateBotEmbed(title="STATS", description=f"<@{user.id}>")
+    embed=BotEmbed(title="STATS", description=f"<@{user.id}>")
     embed.set_thumbnail(url=user.avatar)
     embed.add_field(name="", value=f"**Games played : {wins+lost}\nElo : {elo}\nTop 1 : {top1}\nWins : {wins}\nLost : {lost}\nLast game played : {date}**")
     return await ctx.send(embed=embed)
@@ -231,9 +236,8 @@ async def leaderboard(ctx : commands.Context) -> None:
     return
 #$setup_leaderboard
 @bot.command()
-async def setup_leaderboard(ctx: commands.Context) -> None:
-    channel = bot.get_channel(1204872785642389575)
-    await setup_scoreboard(ctx, channel)
+async def update_leaderboard(ctx : commands.Context) -> None:
+    await update_scoreboard(bot)
     return
 
 #$add_user
@@ -241,15 +245,15 @@ async def setup_leaderboard(ctx: commands.Context) -> None:
 async def add_user(ctx : commands.Context, user : discord.User) -> None:
     caller : discord.User = ctx.message.author
     if (caller.id != 866997795993944084):
-        embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
+        embed = ErrorEmbed(title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
         await caller.send(embed=embed)
         return
     else:
         result : int = add_u(user)
         if (result):
-            embed = CivPrivateBotEmbed(colour=discord.Colour.green(), title="USER ADDED", description=f"{user.mention} successfully added.")
+            embed = SuccessEmbed(colour=discord.Colour.green(), title="USER ADDED", description=f"{user.mention} successfully added.")
         else:
-            embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title=f"ERROR", description="Error during the adding process.")
+            embed = ErrorEmbed(description=f"{user.mention} is already in the database.")
         await ctx.send(embed=embed)
         return
 #$rm_user
@@ -257,15 +261,15 @@ async def add_user(ctx : commands.Context, user : discord.User) -> None:
 async def rm_user(ctx : commands.Context, user : discord.User) -> None:
     caller : discord.User = ctx.message.author
     if (caller.id != 866997795993944084):
-        embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
+        embed = ErrorEmbed(title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
         await caller.send(embed=embed)
         return
     else:
         result : int = rm_u(user)
         if (result):
-            embed = CivPrivateBotEmbed(colour=discord.Colour.green(), title="USER DELETED", description=f"{user.mention} successfully deleted.")
+            embed = SuccessEmbed(description=f"{user.mention} successfully deleted.")
         else:
-            embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="ERROR", description="Error during the deleting process.")
+            embed = ErrorEmbed(description=f"{user.mention} is already not in the database.")
         await ctx.send(embed=embed)
         return
 #$clear_leaderboard
@@ -273,18 +277,18 @@ async def rm_user(ctx : commands.Context, user : discord.User) -> None:
 async def clear_leaderboard(ctx : commands.Context) -> None:
     caller : discord.User = ctx.message.author
     if (caller.id != 866997795993944084):
-        embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
+        embed = ErrorEmbed(title="PERMISSIONS ISSUE", description="You're not allowed to use this command.")
         await caller.send(embed=embed)
         return
     else:
         if (not is_database_empty()):
             result = rm_all_users()
             if (result):
-                embed = CivPrivateBotEmbed(colour=discord.Colour.green(), title="LEADERBOARD CLEARED", description=f"Leaderboard successfully cleared.")
+                embed = SuccessEmbed(description=f"Leaderboard successfully cleared.")
             else:
-                embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="ERROR", description="Error during the leaderboard clearing process.")
+                embed = ErrorEmbed(description="Error during the leaderboard clearing process.")
         else:
-            embed = CivPrivateBotEmbed(colour=discord.Colour.red(), title="ERROR", description="Database is already empty.")
+            embed = ErrorEmbed(description="Database is already empty.")
         await ctx.send(embed=embed)
         return
 
