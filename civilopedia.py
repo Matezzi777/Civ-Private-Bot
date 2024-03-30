eb0d78d2a192a320ed59140a5b904da3bc2808c6
Matezzi777 <matezzi777@gmail.com>
2024-03-31 00:54:56 +0100

    civilopedia project based

--- civilopedia.py
+++ civilopedia.py
@@ -1,12 +1,2 @@
 #============================================= INITIALISATION ===============================================
 #Import des modules
-import discord
-import discord.ext
-import requests
-from bs4 import BeautifulSoup
-from classes import Bot, BotEmbed, SuccessEmbed, ErrorEmbed
-
-url = 'https://www.civilopedia.net/fr/gathering-storm/civilizations/civilization_france'
-
-requests.get(url)
-
